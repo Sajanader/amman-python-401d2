d9b36542cd6088440e031a7dc711f353f2e78b16
Ahmad Alawad <ahmad.alawad.sf@gmail.com>
2020-11-29 13:16:21 +0200

    added demo for class 01

---
+++ class-01/demo/class-one-topics/class_one_topics/importer.py
@@ -0,0 +1,4 @@
+from topics import input_output, some_secret_num
+
+input_output()
+print('Secret Num: ', some_secret_num)
---
+++ class-01/demo/first_program.py
@@ -0,0 +1,7 @@
+x = 5
+y = 8
+print('multiplication', x*y)
+
+choice = input('Enter your choice: ')
+
+print(choice)
