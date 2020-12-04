243c3e2d102ccf54e10d06b43963d95f2e928d18
Sajanader <sajanader93@gmail.com>
2020-12-05 00:23:16 +0200

    adding

--- data-structures-and-algorithms-python/data/challenges/array_reverse/array_reverse.py
+++ data-structures-and-algorithms-python/data/challenges/array_reverse/array_reverse.py
@@ -1,8 +1,8 @@
 
-def reverse_array(arr):
+arr=[1,2,3,4,5]
+def reverse_array(): 
     reversedArr=[]
-    for i in arr
-        reverse_array.insert(0,i)
-        return reversedArr
-
-print(reverse_array(arr))        
+    for i in arr:
+        reversedArr.insert(0,i)
+        print(reversedArr)
+reverse_array() 
