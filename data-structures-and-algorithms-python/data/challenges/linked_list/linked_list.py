336e694e66b4825f3ca4ab9a6f23daab69565f7a
Sajanader <sajanader93@gmail.com>
2020-12-06 23:52:16 +0200

    challenge-06

--- data-structures-and-algorithms-python/data/challenges/linked_list/linked_list.py
+++ data-structures-and-algorithms-python/data/challenges/linked_list/linked_list.py
@@ -3,12 +3,9 @@ class Node:
         self.value = value
         self.next = None
 
-
-
 class LinkedList:
     def __init__(self):
         self.head = None
-
     def insert(self, data):
         node = Node(data)
         if self.head == None:
@@ -16,7 +13,6 @@ class LinkedList:
         else:
             node.next = self.head
             self.head = node
-
     def includes(self, data):
         current = self.head
         while (current != None):
@@ -24,7 +20,6 @@ class LinkedList:
                 return True
             else:
                 return False
-
     def __str__(self):
         if self.head == None:
             return ("NULL")
@@ -36,4 +31,37 @@ class LinkedList:
                 current = current.next
             list += 'NULL'
             return (f"{list}")
+
+    def append(self,data):
+        node = Node(data)
+        if self.head == None:
+           self.head = node
+        else:
+            current = self.head
+            while current.next != None:
+                  current = current.next
+            current.next= node
+    
+    def insertBefore(self,value,newVal):
+        node = Node(newVal)
+        if (self.head.value == value):
+            return(self.insert(newVal))
+        current = self.head
+        while(current.next.value != value):
+            current = current.next
+        node.next = current.next
+        current.next = node
+
+    def insertAfter(self,value,newVal):
+        node = Node(newVal)
+        current = self.head
+        while current.value != value:
+              current = current.next
+        node.next = current.next
+        current.next = node
+
+
+
+
+
 
