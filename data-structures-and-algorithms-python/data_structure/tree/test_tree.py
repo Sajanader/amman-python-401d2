9038ece0172101b8dff323e14f03178715a5d6ce
Sajanader <sajanader93@gmail.com>
2020-12-20 09:02:27 +0200

    solving

---
+++ data-structures-and-algorithms-python/data_structure/tree/test_tree.py
@@ -0,0 +1,23 @@
+from tree import Node , BinaryTree , BinarySearchTree
+import pytest 
+
+@pytest.fixture
+def prep():
+    Binary_tree = BinaryTree()
+    Binary_tree.root = Node(3)
+    Binary_tree.root.right = Node(4)
+    Binary_tree.root.left = Node(1)
+    Binary_tree.root.right.left = Node(2)
+    return Binary_tree
+
+
+def test_preorder(prep):
+    actual = prep.preorder()  
+    expected = [3,1,4,2]  
+    assert actual == expected 
+
+def test_contains():
+    Binary_tree = BinarySearchTree()
+    Binary_tree.add(2)
+    assert Binary_tree.root.value == 2
+   
---
+++ data-structures-and-algorithms-python/data_structure/tree/tree.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self,value ):
+        self.value  = value
+        self.right = None
+        self.left = None
+
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None 
+
+    def preorder(self):
+        array_of_output = [] 
+
+        def _walk(node):
+            array_of_output.append(node.value)
+            if node.left:
+               _walk(node.left)
+            if node.right:
+               _walk(node.right)  
+        _walk(self.root)  
+ 
+        return array_of_output 
+                
+
+
+
+
+class BinarySearchTree(BinaryTree):
+    def add(self, value):
+        if not self.root:
+            self.root = Node(value)
+        else:
+            def _walk(node):
+                if value < node.value:
+                    if not node.left:
+                        node.left = Node(value)
+                        return 
+                    else:
+                        _walk(node.left) 
+                else:
+                    if not node.right:
+                        node.right = Node(value)
+                        return 
+                    else:
+                        _walk(node.right)
+                _walk(self.root)
+
+    def contains(self, value):
+        if self.root.value == value:
+            return True
+        elif self.root.value < value:
+            return self.root.right.value == value 
+        else:
+            return self.root.right.value == value      
+    
+
+
+if __name__ == "__main__":
+    bt = BinaryTree()
+    bt.root = Node(6)
+    bt.root.right = Node(5)
+    bt.root.left = Node(-1)
+    bt.root.right.left = Node(7)
+    bt.root.left.left = Node(10)
+    bt.root.right.right = Node(3)
+    print(bt.preorder())
+ 
+
+
+
