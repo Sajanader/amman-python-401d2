64bff517307588616b6feefcc5a308e8ac7f2800
Saja Nader Alsmael <70093999+Sajanader@users.noreply.github.com>
2020-12-21 09:26:35 +0200

    Update test_tree.py

--- data-structures-and-algorithms-python/data_structure/tree/test_tree.py
+++ data-structures-and-algorithms-python/data_structure/tree/test_tree.py
@@ -21,25 +21,6 @@ def test_contains():
     Binary_tree.add(2)
     assert Binary_tree.root.value == 2
     
-def test_tree_inorder():
-    Binary_tree = BinaryTree()
-    Binary_tree.root = Node(6)
-    Binary_tree.root.right = Node(5)
-    Binary_tree.root.left = Node(-1)
-    Binary_tree.root.right.left = Node(7)
-    Binary_tree.root.left.left = Node(10)
-    Binary_tree.root.right.right = Node(3)
-    assert Binary_tree.inOrder() == [10, -1, 6, 7, 5, 3]
-
-def test_tree_postorder():
-    Binary_tree = BinaryTree()
-    Binary_tree.root = Node(6)
-    Binary_tree.root.right = Node(5)
-    Binary_tree.root.left = Node(-1)
-    Binary_tree.root.right.left = Node(7)
-    Binary_tree.root.left.left = Node(10)
-    Binary_tree.root.right.right = Node(3)
-    assert Binary_tree.postOrder() == [10, -1, 7, 3, 5, 6]
 
 def test_max_value():
     Binary_tree = BinaryTree()
